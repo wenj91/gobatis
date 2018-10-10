6c7acc5c04e308c5bf3f068cedff91188082093e
wenj91 <1541683150@qq.com>
2018-10-10 17:11:09 +0800

    Create expr_test.go

---
+++ expr_test.go
@@ -0,0 +1,40 @@
+import (
+	"fmt"
+	"testing"
+)
+
+type TestUser struct {
+	Name string
+}
+
+func TestExpr_eval(t *testing.T) {
+	params := map[string]interface{}{
+		"name":"wenj91",
+		"user":&TestUser{Name:"wenj91"},
+		"m": map[string]interface{}{"user":&TestUser{Name:"wenj91"}},
+		"m1": map[string]interface{}{"name":"wenj91"},
+	}
+	expression := []string{
+		"1 != 1",
+		"1 == 1",
+		"name == 'wenj91'",
+		"name != 'wenj91'",
+		"user.Name1 == 'wenj91'",
+		"user.Name == 'wenj91'",
+		"user.Name != 'wenj91'",
+		"user.Name != nil",
+		"user.Name == nil",
+		"m.user.Name != 'wenj91'",
+		"m.user.Name == 'wenj91'",
+		"m1.name == 'wenj91'",
+		"m1.name != 'wenj91'",
+		"m.user.Name == 'wenj91' && 1 == 1",
+		"m.user.Name == 'wenj91' && 1 != 1",
+		"m.user.Name == 'wenj91' || 1 != 1",
+	}
+
+	for _, ex := range expression {
+		ok := eval(ex, params)
+		fmt.Printf("Eexpr:%v >>>> Result:%v \n", ex, ok)
+	}
+}
