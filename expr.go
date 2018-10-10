1244a93d91331062052561ed1792c400630e2161
wenj91 <1541683150@qq.com>
2018-10-10 17:10:21 +0800

    Update expr.go

--- expr.go
+++ expr.go
@@ -1,14 +1,17 @@
 package gobatis
 
 import (
-	"github.com/araddon/qlbridge/expr"
-	"github.com/araddon/qlbridge/datasource"
-	"github.com/araddon/qlbridge/vm"
+	"github.com/antonmedv/expr"
+	"log"
 )
 
-func exprProcess(expression string, mapper map[string]interface{})  bool  {
-	exprAst := expr.MustParse(expression)
-	evalContext := datasource.NewContextSimpleNative(mapper)
-	v, _ := vm.Eval(evalContext, exprAst)
-	return v.Value().(bool)
+func eval(expression string, mapper map[string]interface{}) bool {
+	ok, err := expr.Eval(expression, mapper)
+	if nil != err {
+		// panic here or better ??
+		log.Println(expression, "eval result err:", err)
+		return false
+	}
+
+	return ok.(bool)
 }
