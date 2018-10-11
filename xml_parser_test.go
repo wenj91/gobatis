2d99c2cb8a6c103b62244edc5a757dd5a9331a8a
wenj91 <1541683150@qq.com>
2018-10-11 18:11:29 +0800

    Update xml_parser_test.go

--- xml_parser_test.go
+++ xml_parser_test.go
@@ -1,24 +1,31 @@
 package gobatis
 
 import (
-	"encoding/json"
 	"fmt"
 	"strings"
 	"testing"
 )
 
-func TestXmlNode_parse(t *testing.T)  {
+func TestXmlNode_parse(t *testing.T) {
 	xmlStr := `
 <?xml version="1.0" encoding="utf-8"?>
 <mapper namespace="Mapper">
     <select id="findMapById" resultType="Map">
-		SELECT id, name FROM user where id=#{id} order by id
+		SELECT id, name FROM user where id=#{id} 
+		<if test="name != nil">
+			<for item="item" open="AND name in (" close=")" separator="," collection="names">
+				#{item}
+			</for>
+			AND name = #{name}
+		</if>
+		ORDER BY id
     </select>
+	<update id="updateById">
+		UPDATE t_gap SET gap = #{gap} WHERE id = #{id}
+	</update>
 </mapper>
 `
 	r := strings.NewReader(xmlStr)
-
-	node := parse(r)
-	res, _ := json.Marshal(node)
-	fmt.Println(string(res))
+	rn := parse(r)
+	fmt.Println(rn)
 }
