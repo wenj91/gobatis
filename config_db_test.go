08a5e805fa52b659f72b895c1a58f03a2e35b0df
wenj91 <1541683150@qq.com>
2018-10-12 16:21:13 +0800

    Update config_db_test.go

--- config_db_test.go
+++ config_db_test.go
@@ -10,8 +10,9 @@ func TestDbConfig(t *testing.T)  {
 db:
   driverName: mysql
   dataSourceName: root:123456@tcp(127.0.0.1:3306)/test?charset=utf8
-  maxLifeTime: 10
-  maxIdleConns: 1
+  maxLifeTime: 120
+  maxOpenConns: 10
+  maxIdleConns: 5
 mappers:
   - userMapper.xml
   - orderMapper.xml
@@ -20,8 +21,9 @@ mappers:
 
 	assertEqual(dbconf.DB.DriverName, "mysql", "test fail, actual:" + dbconf.DB.DriverName)
 	assertEqual(dbconf.DB.DataSourceName, "root:123456@tcp(127.0.0.1:3306)/test?charset=utf8", "test fail, actual:" + dbconf.DB.DataSourceName)
-	assertEqual(dbconf.DB.MaxLifeTime, 10, "test fail, actual:" + fmt.Sprintf("%d", dbconf.DB.MaxLifeTime))
-	assertEqual(dbconf.DB.MaxIdleConns, 1, "test fail, actual:" + fmt.Sprintf("%d", dbconf.DB.MaxIdleConns))
+	assertEqual(dbconf.DB.MaxLifeTime, 120, "test fail, actual:" + fmt.Sprintf("%d", dbconf.DB.MaxLifeTime))
+	assertEqual(dbconf.DB.MaxOpenConns, 10, "test fail, actual:" + fmt.Sprintf("%d", dbconf.DB.MaxOpenConns))
+	assertEqual(dbconf.DB.MaxIdleConns, 5, "test fail, actual:" + fmt.Sprintf("%d", dbconf.DB.MaxIdleConns))
 	assertTrue(len(dbconf.Mappers) == 2, "len(dbconf.Mappers) != 2")
 	assertEqual(dbconf.Mappers[0], "userMapper.xml", "test fail, actual:" + dbconf.Mappers[0])
 	assertEqual(dbconf.Mappers[1], "orderMapper.xml", "test fail, actual:" + dbconf.Mappers[1])
