047c631686fb5f85488dfd4649500faf336834ff
wenj91 <1541683150@qq.com>
2018-10-12 18:20:33 +0800

    Update gobatis.go

--- gobatis.go
+++ gobatis.go
@@ -6,6 +6,24 @@ import (
 	"fmt"
 )
 
+type ResultType string
+
+const (
+	// result set is a map: map[string]interface{}
+	resultTypeMap ResultType = "Map"
+	// result set is a slice, item is map: []map[string]interface{}
+	resultTypeMaps ResultType = "Maps"
+	// result set is a struct
+	resultTypeStruct ResultType = "Struct"
+	// result set is a slice, item is struct
+	resultTypeStructs ResultType = "Structs"
+	// result set is a value slice, []interface{}
+	resultTypeSlice ResultType = "Slice"
+	// result set is a value slice, item is value slice, []interface{}
+	resultTypeSlices ResultType = "Slices"
+	resultTypeValue  ResultType = "Value"
+)
+
 // reference from https://github.com/yinshuwei/osm/blob/master/osm.go start
 type dbRunner interface {
 	Prepare(query string) (*sql.Stmt, error)
