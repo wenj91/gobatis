fb1d4ced21b47e5a6de1cafb9b96ba5030598fc3
wenj91 <1541683150@qq.com>
2018-10-12 16:19:10 +0800

    Update config_db.go

--- config_db.go
+++ config_db.go
@@ -5,6 +5,7 @@ type dbConfig struct {
 		DriverName     string `yaml:"driverName"`
 		DataSourceName string `yaml:"dataSourceName"`
 		MaxLifeTime    int    `yaml:"maxLifeTime"`
+		MaxOpenConns   int    `yaml:"maxOpenConns"`
 		MaxIdleConns   int    `yaml:"maxIdleConns"`
 	} `yaml:"db"`
 	Mappers []string `yaml:"mappers"`
