e7fc41d8a434a82f69004555abfb6e1f7208f1d8
wenj91 <1541683150@qq.com>
2018-10-15 16:52:10 +0800

    Update proc_res.go

--- proc_res.go
+++ proc_res.go
@@ -3,6 +3,7 @@ package gobatis
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 	"log"
 	"reflect"
 )
@@ -10,14 +11,108 @@ import (
 type resultTypeProc = func(rows *sql.Rows, res interface{}) error
 
 var resSetProcMap = map[ResultType]resultTypeProc{
-	resultTypeMap:    resMapProc,
-	resultTypeMaps:   resMapsProc,
-	resultTypeSlice:  resSliceProc,
-	resultTypeSlices: resSlicesProc,
-	resultTypeValue:  resValueProc,
+	resultTypeMap:     resMapProc,
+	resultTypeMaps:    resMapsProc,
+	resultTypeSlice:   resSliceProc,
+	resultTypeSlices:  resSlicesProc,
+	resultTypeValue:   resValueProc,
+	resultTypeStructs: resStructsProc,
+	resultTypeStruct:  resStructProc,
+}
+
+func resStructProc(rows *sql.Rows, res interface{}) error {
+	resVal := reflect.ValueOf(res)
+	if resVal.Kind() != reflect.Ptr {
+		return errors.New("Struct query result must be ptr")
+	}
+
+	resVal = resVal.Elem()
+	if resVal.Kind() != reflect.Struct {
+		return errors.New("Struct query result must be struct")
+	}
+
+	arr, err := rowsToStructs(rows, resVal.Type())
+	if nil != err {
+		return err
+	}
+
+	if len(arr) > 1 {
+		return errors.New("Struct query result more than one row")
+	}
+
+	if len(arr) > 0 {
+		resVal.Set(reflect.ValueOf(arr[0]))
+	}
+
+	return nil
+}
+
+func resStructsProc(rows *sql.Rows, res interface{}) error {
+	sliceVal := reflect.ValueOf(res)
+	if sliceVal.Kind() != reflect.Ptr {
+		return errors.New("Structs query result must be ptr")
+	}
+
+	slicePtr := reflect.Indirect(sliceVal)
+	if slicePtr.Kind() != reflect.Slice && slicePtr.Kind() != reflect.Array {
+		return errors.New("Structs query result must be slice")
+	}
+
+	//get elem type
+	elem := slicePtr.Type().Elem()
+	resultType := elem
+	isPtr := elem.Kind() == reflect.Ptr
+	if isPtr {
+		resultType = elem.Elem()
+	}
+
+	if resultType.Kind() != reflect.Struct {
+		return errors.New("Structs query results item must be struct")
+	}
+
+	arr, err := rowsToStructs(rows, resultType)
+	if nil != err {
+		return err
+	}
+
+	for i := 0; i < len(arr); i++ {
+		if isPtr {
+			slicePtr.Set(reflect.Append(slicePtr, reflect.ValueOf(arr[i]).Addr()))
+		} else {
+			slicePtr.Set(reflect.Append(slicePtr, reflect.ValueOf(arr[i])))
+		}
+	}
+
+	return nil
 }
 
 func resValueProc(rows *sql.Rows, res interface{}) error {
+	resPtr := reflect.ValueOf(res)
+	if resPtr.Kind() != reflect.Ptr {
+		return errors.New("Value query result must be ptr")
+	}
+
+	arr, err := rowsToSlices(rows)
+	if nil != err {
+		return err
+	}
+
+	if len(arr) > 1 {
+		return errors.New("Value query result more than one row")
+	}
+
+	tempResSlice := arr[0].([]interface{})
+	if len(tempResSlice) > 1 {
+		return errors.New("Value query result more than one col")
+	}
+
+	if len(tempResSlice) > 0 {
+		if nil != tempResSlice[0] {
+			value := reflect.Indirect(resPtr)
+			value.Set(reflect.ValueOf(tempResSlice[0]))
+		}
+
+	}
 
 	return nil
 }
@@ -188,6 +283,61 @@ func rowsToSlices(rows *sql.Rows) ([]interface{}, error) {
 		}
 
 		res = append(res, resSlice)
+	}
+
+	return res, nil
+}
+
+func rowsToStructs(rows *sql.Rows, resultType reflect.Type) ([]interface{}, error) {
+	fieldsMapper := make(map[string]string)
+	fields := resultType.NumField()
+	for i := 0; i < fields; i++ {
+		field := resultType.Field(i)
+		fieldsMapper[field.Name] = field.Name
+		tag := field.Tag.Get("field")
+		if tag != "" {
+			fieldsMapper[tag] = field.Name
+		}
+	}
+
+	fmt.Println("field mapper:", fieldsMapper)
+
+	res := make([]interface{}, 0)
+	for rows.Next() {
+		cols, err := rows.Columns()
+		if nil != err {
+			log.Fatal(err)
+			return nil, err
+		}
+
+		vals := make([]interface{}, len(cols))
+		scanArgs := make([]interface{}, len(cols))
+		for i := range vals {
+			scanArgs[i] = &vals[i]
+		}
+
+		rows.Scan(scanArgs...)
+
+		obj := reflect.New(resultType).Elem()
+		objPtr := reflect.Indirect(obj)
+		for i := 0; i < len(cols); i++ {
+			colName := cols[i]
+			fieldName := fieldsMapper[colName]
+			field := objPtr.FieldByName(fieldName)
+			//设置相关字段的值,并判断是否可设值
+			if field.CanSet() && vals[i] != nil {
+
+				//获取字段类型并设值
+				data := dataToFieldVal(vals[i], field.Type())
+				if nil != data {
+					field.Set(reflect.ValueOf(data))
+				}
+			}
+		}
+
+		if objPtr.CanInterface() {
+			res = append(res, objPtr.Interface())
+		}
 	}
 
 	return res, nil
