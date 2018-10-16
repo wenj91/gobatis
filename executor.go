517b44b2cb1f21be2d99d36408a3e0b8d311f8c6
wenj91 <1541683150@qq.com>
2018-10-16 09:09:25 +0800

    Update executor.go

--- executor.go
+++ executor.go
@@ -2,16 +2,58 @@ package gobatis
 
 import (
 	"errors"
-	"log"
+	"fmt"
 )
 
 type executor struct {
 	gb *gbBase
 }
 
+func (this *executor) update(ms *mappedStmt, params map[string]interface{}) (lastInsertId int64, affected int64, err error) {
+	boundSql := ms.sqlSource.getBoundSql(params)
+	fmt.Println("SQL:", boundSql.sqlStr)
+	fmt.Println("ParamMappings:", boundSql.paramMappings)
+
+	paramArr := make([]interface{}, 0)
+	for i := 0; i < len(boundSql.paramMappings); i++ {
+		paramName := boundSql.paramMappings[i]
+		param, ok := boundSql.extParams[paramName]
+		if !ok {
+			return 0, 0, errors.New("param:" + paramName + " not exists")
+		}
+
+		paramArr = append(paramArr, param)
+	}
+
+	fmt.Println("Params:", paramArr)
+
+	stmt, err := this.gb.db.Prepare(boundSql.sqlStr)
+	if nil != err {
+		return 0, 0, err
+	}
+
+	result, err := stmt.Exec(paramArr...)
+	if nil != err {
+		return 0, 0, err
+	}
+
+	lastInsertId, err = result.LastInsertId()
+	if nil != err {
+		return 0, 0, err
+	}
+	affected, err = result.RowsAffected()
+	if nil != err {
+		return 0, 0, err
+	}
+
+	return lastInsertId, affected, nil
+}
+
 
 func (this *executor) query(ms *mappedStmt, params map[string]interface{}, res interface{}) error {
 	boundSql := ms.sqlSource.getBoundSql(params)
+	fmt.Println("SQL:", boundSql.sqlStr)
+	fmt.Println("ParamMappings:", boundSql.paramMappings)
 
 	paramArr := make([]interface{}, 0)
 	for i := 0; i < len(boundSql.paramMappings); i++ {
@@ -24,11 +66,7 @@ func (this *executor) query(ms *mappedStmt, params map[string]interface{}, res i
 		paramArr = append(paramArr, param)
 	}
 
-	if conf.dbConf.DB.ShowSql {
-		log.Println("SQL:", boundSql.sqlStr)
-		log.Println("ParamMappings:", boundSql.paramMappings)
-		log.Println("Params:", paramArr)
-	}
+	fmt.Println("Params:", paramArr)
 
 	rows, err := this.gb.db.Query(boundSql.sqlStr, paramArr...)
 	if nil != err {
