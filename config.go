040b99ae10020383a0cbf45743fbef2020af543e
wenj91 <1541683150@qq.com>
2018-10-17 09:32:29 +0800

    Update config.go

--- config.go
+++ config.go
@@ -14,7 +14,7 @@ type config struct {
 }
 
 var conf *config
-var db *sql.DB
+var db map[string]*sql.DB
 
 func ConfInit(dbConfPath string)  {
 	if nil != conf {
@@ -36,19 +36,15 @@ func ConfInit(dbConfPath string)  {
 		log.Fatalln("Read db conf err:", err)
 		return
 	}
+
 	dbConf := buildDbConfig(string(r))
 	if nil == dbConf {
 		log.Fatalln("Build db config err: dbConf == nil")
 		return
 	}
 
-	if dbConf.DB.DriverName == "" {
-		log.Fatalln("Db config err: driverName must not be nil")
-		return
-	}
-
-	if dbConf.DB.DataSourceName == "" {
-		log.Fatalln("Db config err: dataSourceName must not be nil")
+	if len(dbConf.DB) <= 0 {
+		log.Fatalln("No datasource config")
 		return
 	}
 
@@ -75,38 +71,51 @@ func ConfInit(dbConfPath string)  {
 	}
 
 	// init db
-	dbInit()
+	dbInit(dbConf)
 }
 
-func dbInit()  {
-	dbConn, err := sql.Open(conf.dbConf.DB.DriverName, conf.dbConf.DB.DataSourceName)
-	if nil != err {
-		log.Println(err)
-		panic(err)
-	}
+func dbInit(dbConf *dbConfig)  {
+	db = make(map[string]*sql.DB)
+	for k, item := range dbConf.DB {
+		if item.DriverName == "" {
+			log.Fatalln("Db config err: driverName must not be nil")
+			return
+		}
 
-	if err := dbConn.Ping(); err != nil {
-		log.Println(err)
-		panic(err)
-	}
+		if item.DataSourceName == "" {
+			log.Fatalln("Db config err: dataSourceName must not be nil")
+			return
+		}
 
-	if conf.dbConf.DB.MaxLifeTime == 0 {
-		dbConn.SetConnMaxLifetime(120 * time.Second)
-	} else {
-		dbConn.SetConnMaxLifetime(time.Duration(conf.dbConf.DB.MaxLifeTime) * time.Second)
-	}
+		dbConn, err := sql.Open(item.DriverName, item.DataSourceName)
+		if nil != err {
+			log.Println(err)
+			panic(err)
+		}
 
-	if conf.dbConf.DB.MaxOpenConns == 0 {
-		dbConn.SetMaxOpenConns(10)
-	} else {
-		dbConn.SetMaxOpenConns(conf.dbConf.DB.MaxOpenConns)
-	}
+		if err := dbConn.Ping(); err != nil {
+			log.Println(err)
+			panic(err)
+		}
 
-	if conf.dbConf.DB.MaxOpenConns == 0 {
-		dbConn.SetMaxIdleConns(5)
-	} else {
-		dbConn.SetMaxIdleConns(conf.dbConf.DB.MaxIdleConns)
-	}
+		if item.MaxLifeTime == 0 {
+			dbConn.SetConnMaxLifetime(120 * time.Second)
+		} else {
+			dbConn.SetConnMaxLifetime(time.Duration(item.MaxLifeTime) * time.Second)
+		}
 
-	db = dbConn
+		if item.MaxOpenConns == 0 {
+			dbConn.SetMaxOpenConns(10)
+		} else {
+			dbConn.SetMaxOpenConns(item.MaxOpenConns)
+		}
+
+		if item.MaxOpenConns == 0 {
+			dbConn.SetMaxIdleConns(5)
+		} else {
+			dbConn.SetMaxIdleConns(item.MaxIdleConns)
+		}
+
+		db[k] = dbConn
+	}
 }
