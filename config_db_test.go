7ae8cfcd35954f1702453e6566d03dcc13718afb
wenj91 <1541683150@qq.com>
2018-10-17 09:33:27 +0800

    Update config_db_test.go

--- config_db_test.go
+++ config_db_test.go
@@ -8,22 +8,33 @@ import (
 func TestDbConfig(t *testing.T)  {
 	ymlStr := `
 db:
-  driverName: mysql
-  dataSourceName: root:123456@tcp(127.0.0.1:3306)/test?charset=utf8
-  maxLifeTime: 120
-  maxOpenConns: 10
-  maxIdleConns: 5
+  datasource1:
+    driverName: mysql
+    dataSourceName: root:123456@tcp(127.0.0.1:3306)/test?charset=utf8
+    maxLifeTime: 120
+    maxOpenConns: 10
+    maxIdleConns: 5
+  datasource2:
+    driverName: mysql
+    dataSourceName: root:123456@tcp(127.0.0.1:3306)/test?charset=utf8
+    maxLifeTime: 120
+    maxOpenConns: 10
+    maxIdleConns: 5
+showSql: true
 mappers:
   - userMapper.xml
   - orderMapper.xml
 `
 	dbconf := buildDbConfig(ymlStr)
 
-	assertEqual(dbconf.DB.DriverName, "mysql", "test fail, actual:" + dbconf.DB.DriverName)
-	assertEqual(dbconf.DB.DataSourceName, "root:123456@tcp(127.0.0.1:3306)/test?charset=utf8", "test fail, actual:" + dbconf.DB.DataSourceName)
-	assertEqual(dbconf.DB.MaxLifeTime, 120, "test fail, actual:" + fmt.Sprintf("%d", dbconf.DB.MaxLifeTime))
-	assertEqual(dbconf.DB.MaxOpenConns, 10, "test fail, actual:" + fmt.Sprintf("%d", dbconf.DB.MaxOpenConns))
-	assertEqual(dbconf.DB.MaxIdleConns, 5, "test fail, actual:" + fmt.Sprintf("%d", dbconf.DB.MaxIdleConns))
+	dbc, ok := dbconf.DB["datasource1"]
+	assertTrue(ok, "test fail: No datasource1")
+	assertTrue(dbconf.ShowSql, "test fail: showSql == false")
+	assertEqual(dbc.DriverName, "mysql", "test fail, actual:" + dbc.DriverName)
+	assertEqual(dbc.DataSourceName, "root:123456@tcp(127.0.0.1:3306)/test?charset=utf8", "test fail, actual:" + dbc.DataSourceName)
+	assertEqual(dbc.MaxLifeTime, 120, "test fail, actual:" + fmt.Sprintf("%d", dbc.MaxLifeTime))
+	assertEqual(dbc.MaxOpenConns, 10, "test fail, actual:" + fmt.Sprintf("%d", dbc.MaxOpenConns))
+	assertEqual(dbc.MaxIdleConns, 5, "test fail, actual:" + fmt.Sprintf("%d", dbc.MaxIdleConns))
 	assertTrue(len(dbconf.Mappers) == 2, "len(dbconf.Mappers) != 2")
 	assertEqual(dbconf.Mappers[0], "userMapper.xml", "test fail, actual:" + dbconf.Mappers[0])
 	assertEqual(dbconf.Mappers[1], "orderMapper.xml", "test fail, actual:" + dbconf.Mappers[1])
