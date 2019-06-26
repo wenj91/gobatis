fb79ec6c5213b03ff640e3d97efc77c1aaf05e7e
wenj91 <1541683150@qq.com>
2019-06-26 13:14:32 +0800

    u-val upcasting

--- val.go
+++ val.go
@@ -176,6 +176,448 @@ func valToString(data interface{}) string {
 	return s
 }
 
+func valUpcast(data interface{}, typeName string) interface{} {
+	d := data
+	switch typeName {
+	case "bool":
+
+	case "int":
+		switch data.(type) {
+		case bool:
+			if data.(bool) {
+				d = int(1)
+			} else {
+				d = int(0)
+			}
+		case int8:
+			d = int(data.(int8))
+		case int16:
+			d = int(data.(int16))
+		case int32:
+			d = int(data.(int32))
+		case int64:
+			d = int(data.(int64))
+		case uint:
+			d = int(data.(uint))
+		case uint8:
+			d = int(data.(uint8))
+		case uint16:
+			d = int(data.(uint16))
+		case uint32:
+			d = int(data.(uint32))
+		case uint64:
+			d = int(data.(uint64))
+		case uintptr:
+			d = int(data.(uintptr))
+		case float32:
+			d = int(data.(float32))
+		case float64:
+			d = int(data.(float64))
+		}
+	case "int8":
+		switch data.(type) {
+		case bool:
+			if data.(bool) {
+				d = int8(1)
+			} else {
+				d = int8(0)
+			}
+		case int:
+			d = int8(data.(int))
+		case int16:
+			d = int8(data.(int16))
+		case int32:
+			d = int8(data.(int32))
+		case int64:
+			d = int8(data.(int64))
+		case uint:
+			d = int8(data.(uint))
+		case uint8:
+			d = int8(data.(uint8))
+		case uint16:
+			d = int8(data.(uint16))
+		case uint32:
+			d = int8(data.(uint32))
+		case uint64:
+			d = int8(data.(uint64))
+		case uintptr:
+			d = int8(data.(uintptr))
+		case float32:
+			d = int8(data.(float32))
+		case float64:
+			d = int8(data.(float64))
+		}
+	case "int16":
+		switch data.(type) {
+		case bool:
+			if data.(bool) {
+				d = int16(1)
+			} else {
+				d = int16(0)
+			}
+		case int:
+			d = int16(data.(int))
+		case int8:
+			d = int16(data.(int8))
+		case int32:
+			d = int16(data.(int32))
+		case int64:
+			d = int16(data.(int64))
+		case uint:
+			d = int16(data.(uint))
+		case uint8:
+			d = int16(data.(uint8))
+		case uint16:
+			d = int16(data.(uint16))
+		case uint32:
+			d = int16(data.(uint32))
+		case uint64:
+			d = int16(data.(uint64))
+		case uintptr:
+			d = int16(data.(uintptr))
+		case float32:
+			d = int16(data.(float32))
+		case float64:
+			d = int16(data.(float64))
+		}
+	case "int32":
+		switch data.(type) {
+		case bool:
+			if data.(bool) {
+				d = int32(1)
+			} else {
+				d = int32(0)
+			}
+		case int:
+			d = int32(data.(int))
+		case int8:
+			d = int32(data.(int8))
+		case int16:
+			d = int32(data.(int16))
+		case int64:
+			d = int32(data.(int64))
+		case uint:
+			d = int32(data.(uint))
+		case uint8:
+			d = int32(data.(uint8))
+		case uint16:
+			d = int32(data.(uint16))
+		case uint32:
+			d = int32(data.(uint32))
+		case uint64:
+			d = int32(data.(uint64))
+		case uintptr:
+			d = int32(data.(uintptr))
+		case float32:
+			d = int32(data.(float32))
+		case float64:
+			d = int32(data.(float64))
+		}
+	case "int64":
+		switch data.(type) {
+		case bool:
+			if data.(bool) {
+				d = int64(1)
+			} else {
+				d = int64(0)
+			}
+		case int:
+			d = int64(data.(int))
+		case int8:
+			d = int64(data.(int8))
+		case int16:
+			d = int64(data.(int16))
+		case int32:
+			d = int64(data.(int32))
+		case uint:
+			d = int64(data.(uint))
+		case uint8:
+			d = int64(data.(uint8))
+		case uint16:
+			d = int64(data.(uint16))
+		case uint32:
+			d = int64(data.(uint32))
+		case uint64:
+			d = int64(data.(uint64))
+		case uintptr:
+			d = int64(data.(uintptr))
+		case float32:
+			d = int64(data.(float32))
+		case float64:
+			d = int64(data.(float64))
+		}
+	case "uint":
+		switch data.(type) {
+		case bool:
+			if data.(bool) {
+				d = uint(1)
+			} else {
+				d = uint(0)
+			}
+		case int:
+			d = uint(data.(int))
+		case int8:
+			d = uint(data.(int8))
+		case int16:
+			d = uint(data.(int16))
+		case int32:
+			d = uint(data.(int32))
+		case int64:
+			d = uint(data.(int64))
+		case uint8:
+			d = uint(data.(uint8))
+		case uint16:
+			d = uint(data.(uint16))
+		case uint32:
+			d = uint(data.(uint32))
+		case uint64:
+			d = uint(data.(uint64))
+		case uintptr:
+			d = uint(data.(uintptr))
+		case float32:
+			d = uint(data.(float32))
+		case float64:
+			d = uint(data.(float64))
+		}
+	case "uint8":
+		switch data.(type) {
+		case bool:
+			if data.(bool) {
+				d = uint8(1)
+			} else {
+				d = uint8(0)
+			}
+		case int:
+			d = uint8(data.(int))
+		case int8:
+			d = uint8(data.(int8))
+		case int16:
+			d = uint8(data.(int16))
+		case int32:
+			d = uint8(data.(int32))
+		case int64:
+			d = uint8(data.(int64))
+		case uint:
+			d = uint8(data.(uint))
+		case uint16:
+			d = uint8(data.(uint16))
+		case uint32:
+			d = uint8(data.(uint32))
+		case uint64:
+			d = uint8(data.(uint64))
+		case uintptr:
+			d = uint8(data.(uintptr))
+		case float32:
+			d = uint8(data.(float32))
+		case float64:
+			d = uint8(data.(float64))
+		}
+	case "uint16":
+		switch data.(type) {
+		case bool:
+			if data.(bool) {
+				d = uint16(1)
+			} else {
+				d = uint16(0)
+			}
+		case int:
+			d = uint16(data.(int))
+		case int8:
+			d = uint16(data.(int8))
+		case int16:
+			d = uint16(data.(int16))
+		case int32:
+			d = uint16(data.(int32))
+		case int64:
+			d = uint16(data.(int64))
+		case uint:
+			d = uint16(data.(uint))
+		case uint8:
+			d = uint16(data.(uint8))
+		case uint32:
+			d = uint16(data.(uint32))
+		case uint64:
+			d = uint16(data.(uint64))
+		case uintptr:
+			d = uint16(data.(uintptr))
+		case float32:
+			d = uint16(data.(float32))
+		case float64:
+			d = uint16(data.(float64))
+		}
+	case "uint32":
+		switch data.(type) {
+		case bool:
+			if data.(bool) {
+				d = uint32(1)
+			} else {
+				d = uint32(0)
+			}
+		case int:
+			d = uint32(data.(int))
+		case int8:
+			d = uint32(data.(int8))
+		case int16:
+			d = uint32(data.(int16))
+		case int32:
+			d = uint32(data.(int32))
+		case int64:
+			d = uint32(data.(int64))
+		case uint:
+			d = uint32(data.(uint))
+		case uint8:
+			d = uint32(data.(uint8))
+		case uint16:
+			d = uint32(data.(uint16))
+		case uint64:
+			d = uint32(data.(uint64))
+		case uintptr:
+			d = uint32(data.(uintptr))
+		case float32:
+			d = uint32(data.(float32))
+		case float64:
+			d = uint32(data.(float64))
+		}
+	case "uint64":
+		switch data.(type) {
+		case bool:
+			if data.(bool) {
+				d = uint64(1)
+			} else {
+				d = uint64(0)
+			}
+		case int:
+			d = uint64(data.(int))
+		case int8:
+			d = uint64(data.(int8))
+		case int16:
+			d = uint64(data.(int16))
+		case int32:
+			d = uint64(data.(int32))
+		case int64:
+			d = uint64(data.(int64))
+		case uint:
+			d = uint64(data.(uint))
+		case uint8:
+			d = uint64(data.(uint8))
+		case uint16:
+			d = uint64(data.(uint16))
+		case uint32:
+			d = uint64(data.(uint32))
+		case uintptr:
+			d = uint64(data.(uintptr))
+		case float32:
+			d = uint64(data.(float32))
+		case float64:
+			d = uint64(data.(float64))
+		}
+	case "uintptr":
+		switch data.(type) {
+		case bool:
+			if data.(bool) {
+				d = uintptr(1)
+			} else {
+				d = uintptr(0)
+			}
+		case int:
+			d = uintptr(data.(int))
+		case int8:
+			d = uintptr(data.(int8))
+		case int16:
+			d = uintptr(data.(int16))
+		case int32:
+			d = uintptr(data.(int32))
+		case int64:
+			d = uintptr(data.(int64))
+		case uint:
+			d = uintptr(data.(uint))
+		case uint8:
+			d = uintptr(data.(uint8))
+		case uint16:
+			d = uintptr(data.(uint16))
+		case uint32:
+			d = uintptr(data.(uint32))
+		case uint64:
+			d = uintptr(data.(uint64))
+		case float32:
+			d = uintptr(data.(float32))
+		case float64:
+			d = uintptr(data.(float64))
+		}
+	case "float32":
+		switch data.(type) {
+		case bool:
+			if data.(bool) {
+				d = float32(1)
+			} else {
+				d = float32(0)
+			}
+		case int:
+			d = float32(data.(int))
+		case int8:
+			d = float32(data.(int8))
+		case int16:
+			d = float32(data.(int16))
+		case int32:
+			d = float32(data.(int32))
+		case int64:
+			d = float32(data.(int64))
+		case uint:
+			d = float32(data.(uint))
+		case uint8:
+			d = float32(data.(uint8))
+		case uint16:
+			d = float32(data.(uint16))
+		case uint32:
+			d = float32(data.(uint32))
+		case uint64:
+			d = float32(data.(uint64))
+		case uintptr:
+			d = float32(data.(uintptr))
+		case float64:
+			d = float32(data.(float64))
+		}
+	case "float64":
+		switch data.(type) {
+		case bool:
+			if data.(bool) {
+				d = float64(1)
+			} else {
+				d = float64(0)
+			}
+		case int:
+			d = float64(data.(int))
+		case int8:
+			d = float64(data.(int8))
+		case int16:
+			d = float64(data.(int16))
+		case int32:
+			d = float64(data.(int32))
+		case int64:
+			d = float64(data.(int64))
+		case uint:
+			d = float64(data.(uint))
+		case uint8:
+			d = float64(data.(uint8))
+		case uint16:
+			d = float64(data.(uint16))
+		case uint32:
+			d = float64(data.(uint32))
+		case uint64:
+			d = float64(data.(uint64))
+		case uintptr:
+			d = float64(data.(uintptr))
+		case float32:
+			d = float64(data.(float32))
+		}
+	case "complex64":
+	case "complex128":
+
+	}
+
+	return d
+}
+
 func dataToFieldVal(data interface{}, tp reflect.Type, fieldName string) interface{} {
 	defer func() {
 		if err := recover(); nil != err {
@@ -212,6 +654,8 @@ func dataToFieldVal(data interface{}, tp reflect.Type, fieldName string) interfa
 			if dataTp.Kind() == reflect.String {
 				data = stringToVal(data, tp)
 			}
+
+			data = valUpcast(data, typeName)
 
 			return data
 		}
